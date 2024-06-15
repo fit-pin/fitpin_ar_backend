1e15db83536ce8aaf7ae4762622a126d146b173d
Lseoksee <da864268@naver.com>
2024-06-16 00:45:13 +0900

    리눅스 실행 수정

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
+# 이전 스크린 지우기
+screen -X -S fitpin kill
 git pull
-uvicorn src.Server:server --reload --port 8080 --host 0.0.0.0
+# 스크린을 백그라운드로 시작
+screen -dmS fitpin bash -c 'uvicorn src.Server:server --reload --port 8080 --host 0.0.0.0'
