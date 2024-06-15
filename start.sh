1e09f0f8b12e3f65fa2222b4832fd7a8fe02bd6a
Lseoksee <da864268@naver.com>
2024-06-16 08:56:02 +0900

    배쉬 수정

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # 이전 스크린 지우기
 screen -X -S fitpin kill
 git pull
-# 스크린을 백그라운드로 시작
-screen -dmS fitpin -L -Logfile log.txt bash -c 'uvicorn src.Server:server --reload --port 8080 --host 0.0.0.0'
+# 스크린을 bash 명령으로 실행
+screen -S fitpin -L -Logfile log.txt bash -c 'uvicorn src.Server:server --reload --port 8080 --host 0.0.0.0'
