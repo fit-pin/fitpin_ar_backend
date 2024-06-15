97bac089e5bd1362f106e20c88677302eb0c6312
Lseoksee <da864268@naver.com>
2024-06-16 01:03:01 +0900

    로그 파일 남기도록 설정

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 screen -X -S fitpin kill
 git pull
 # 스크린을 백그라운드로 시작
-screen -dmS fitpin bash -c 'uvicorn src.Server:server --reload --port 8080 --host 0.0.0.0'
+screen -dmS fitpin -L -Logfile log.txt bash -c 'uvicorn src.Server:server --reload --port 8080 --host 0.0.0.0'
