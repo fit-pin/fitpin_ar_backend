6f5dd22ff6691753d353150470a3a95666653646
Lseoksee <da864268@naver.com>
2024-07-28 17:58:14 +0900

    쉘 스크립트 업데이트

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-# 이전 스크린 지우기
-screen -X -S fitpin kill
-
 git pull
 
 # 패키지 설치
@@ -9,4 +6,4 @@ conda activate ./.conda
 conda env update -p .conda --prune
 
 # 스크린을 bash 명령으로 실행
-screen -S fitpin -L -Logfile log.txt bash -c 'uvicorn src.Server:server --reload --port 8080 --host 0.0.0.0'
+uvicorn src.Server:server --reload --port 8080 --host 0.0.0.0
