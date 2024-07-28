728d80e51b7fb36a35fbbdbbaf060e6fd8188e6d
Lseoksee <da864268@naver.com>
2024-07-28 18:35:17 +0900

    주석 수정

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ./.conda
 conda env update -p .conda --prune
 
-# 스크린을 bash 명령으로 실행
+# 서버 실행
 uvicorn src.Server:server --reload --port 8080 --host 0.0.0.0
