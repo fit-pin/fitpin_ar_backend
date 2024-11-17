fc1ddb1bd32f296bdcc9e4f683e64bc45ba8b861
Lseoksee <da864268@naver.com>
2024-11-17 21:29:41 +0900

    리버스 프록시 최종 문제 해결

--- src/Server.py
+++ src/Server.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from api import BodyMEA, ClothesMEA, TryOn, GetNukki, Payment
 
-server = FastAPI(redoc_url=None)
+# root-path 리버스 프록시 uri와 동일하게
+server = FastAPI(redoc_url=None, root_path="/ar-api")
 
 server.include_router(prefix="/bodymea", router=BodyMEA.router)
 server.include_router(prefix="/clothesmea", router=ClothesMEA.router)
--- start.sh
+++ start.sh
@@ -6,5 +6,4 @@ conda activate ./.conda
 conda env update -p .conda --prune
 
 # 서버 실행
-# --root-path 리버스 프록시 uri와 동일하게
-uvicorn Server:server --reload --port 80 --host 0.0.0.0 --log-config ./src/log-config.yml --app-dir ./src --root-path /ar-api
+uvicorn Server:server --reload --port 80 --host 0.0.0.0 --log-config ./src/log-config.yml --app-dir ./src
